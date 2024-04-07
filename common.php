b62d06edb2a274ecaf8f3b80768d7632b9ddc0ae
Alexandre LE PAPE <alex@alepape.net>
2024-04-07 12:36:59 +0200

    no logs

--- common.php
+++ common.php
@@ -8,7 +8,7 @@ define("LOGPATH",__DIR__ . "/log/log.txt");
 
 function logMe($txt){
     $datetime = new DateTime();
-	file_put_contents(LOGPATH, $datetime->format(DateTime::ATOM)." - ".$txt."\n", FILE_APPEND);
+	//file_put_contents(LOGPATH, $datetime->format(DateTime::ATOM)." - ".$txt."\n", FILE_APPEND);
 } // TODO: have several levels of logs
 // TODO: expose logs in the UI
 // TODO: fix folder permissions so config.json and log.txt can be created
